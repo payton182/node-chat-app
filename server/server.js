423d7ea70805b5a053abdd7ca11ca844a4720e1d
Payton Clark <payton182@gmail.com>
2019-01-07 16:23:37 -0600

    events

--- server/server.js
+++ server/server.js
@@ -13,9 +13,24 @@ let io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('A new user has connected.')
+  console.log('A new user has connected.');
+
+  socket.emit('newMessage', {
+    from: 'Payton',
+    text: 'Message emit test server'
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
+//If connection is closed
+  socket.on('disconnect', () => {
+    console.log('User was disconnected.');
+  })
 });
 
+//App init
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
