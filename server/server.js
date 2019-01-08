fb83948a6dbe86a9f628bf5d803240bc89faece9
Payton Clark <payton182@gmail.com>
2019-01-07 22:27:49 -0600

    update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express')
 const socketIO = require('socket.io');
 
-
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
@@ -15,23 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('A new user has connected.');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app.'
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'A new user has joined.',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has joined.'));
 
   socket.on('createMessage', (message) => {
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
 //if connection is closed
